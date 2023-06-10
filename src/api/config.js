e6a5a300f0dc9620fd99b9df0b77f80386a0cb6c
limadan <dbatistalima135@gmail.com>
2023-06-10 12:28:03 -0300

    dockerfile

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3030',
+    baseURL: 'get-a-pet-api-container',
     headers: {
         Authorization : `Bearer ${localStorage.getItem("TOKEN")}`
     }
